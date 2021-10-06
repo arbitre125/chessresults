b5069267006296f0cc7a07e0d9dc246b49072a42
Roger Marsh <roger.marsh@solentware.co.uk>
2021-10-06 08:19:52 +0000

    Add pyproject.toml and setup.cfg for 'python -m pip ...' commands.
    setup.py retained for 'python setup.py ...' commands until this is no
    longer supported, but modified to rely on setup.cfg.
    
    Bump version number with .dev0 suffix to test providing a URL, or path
    to a html file, containing links to archives for the --find-links
    argument to 'python -m pip ...' commands.

--- setup.py
+++ setup.py
@@ -1,67 +1,40 @@
 # setup.py
 # Copyright 2011 Roger Marsh
 # Licence: See LICENCE (BSD licence)
-"""chessresults setup file."""
+"""chessresults setup file.
+
+Retained so 'python setup.py ...' can be used by my build scripts until
+conversion to 'python -m pip ...'.
+
+All setup() arguments except dependency_links are set in setup.cfg file.
+"""
 
 from setuptools import setup
+import configparser
 
 if __name__ == "__main__":
 
-    long_description = open("README").read()
-    install_requires=[
-        "solentware-base==4.1.6",
-        "chesscalc==1.2.3",
-        "emailstore==1.4.4",
-        "emailextract==0.7.4",
-        "solentware-grid==2.1.4",
-        "solentware-misc==1.3.2",
-    ]
+    setup_args = {}
 
-    setup(
-        name="chessresults",
-        version="5.0.3",
-        description="Results database for chess games",
-        author="Roger Marsh",
-        author_email="roger.marsh@solentware.co.uk",
-        url="http://www.solentware.co.uk",
-        packages=[
-            "chessresults",
-            "chessresults.core",
-            "chessresults.help",
-            "chessresults.basecore",
-            "chessresults.minorbases",
-            "chessresults.gui",
-            "chessresults.gui.minorbases",
-            "chessresults.berkeleydb",
-            "chessresults.db",
-            "chessresults.dpt",
-            "chessresults.sqlite",
-            "chessresults.apsw",
-            "chessresults.unqlite",
-            "chessresults.vedis",
-            "chessresults.tools",
-        ],
-        package_data={
-            "chessresults.help": ["*.txt"],
-        },
-        long_description=long_description,
-        license="BSD",
-        classifiers=[
-            "License :: OSI Approved :: BSD License",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Operating System :: OS Independent",
-            "Topic :: Games/Entertainment :: Board Games",
-            "Intended Audience :: End Users/Desktop",
-            "Development Status :: 3 - Alpha",
-        ],
-        install_requires=install_requires,
-        dependency_links=[
-            "-".join(required.split("==")).join(
-                ("http://solentware.co.uk/files/", ".tar.gz")
-            )
-            for required in install_requires
-        ],
-    )
+    # Derive dependency_links from install_requires for use by setup().
+    # dependency_links is ignored by non-legacy pip commands; and these
+    # packages have to be installed by separate commands quoting the URL
+    # in the command line.  This code works because all version conditions
+    # are '=='.
+    parser = configparser.ConfigParser()
+    parser.read("setup.cfg")
+    for section in parser.sections():
+        if section != "options":
+            continue
+        for key, value in parser.items(section):
+            if key == "install_requires":
+                install_requires = [v for v in value.splitlines() if v]
+                setup_args["dependency_links"] = [
+                    "-".join(required.split("==")).join(
+                        ("http://solentware.co.uk/files/", ".tar.gz")
+                    )
+                    for required in install_requires
+                ]
+                break
+
+    setup(**setup_args)
