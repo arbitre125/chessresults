ed52e97f57927f36b62a5d70f3c9807518d9fcc7
Roger Marsh <roger.marsh@solentware.co.uk>
2022-08-22 08:39:55 +0000

    Change Configuration class to customise Configuration class from solentware_misc project.
    Change setup.cfg to note new version of chesscalc dependency.

--- chessresults/core/configuration.py
+++ chessresults/core/configuration.py
@@ -2,23 +2,24 @@
 # Copyright 2022 Roger Marsh
 # Licence: See LICENCE (BSD licence)
 
-"""Access and update names of last used database and configuration files.
+"""AAccess and update items in a configuration file.
 
-The initial values are taken from file named in self._RESULTS_CONF in the
+The initial values are taken from file named in self._CONFIGURATION in the
 user's home directory if the file exists.
 
 """
+from solentware_misc.core import configuration
 
-from chessvalidate.core import configuration
+import ecfformat.core.constants
 
-from ..core import constants
+from . import constants
 
 
 class Configuration(configuration.Configuration):
     """Identify configuration and recent files and delegate to superclass."""
 
-    _RESULTS_CONF = ".chessresults.conf"
-    _DEFAULT_RECENTS = (
+    _CONFIGURATION = ".chessresults.conf"
+    _DEFAULT_ITEM_VAULES = (
         (constants.RECENT_DATABASE, "~"),
         (constants.RECENT_EMAIL_SELECTION, "~"),
         (constants.RECENT_EMAIL_EXTRACTION, "~"),
@@ -37,4 +38,9 @@ class Configuration(configuration.Configuration):
         (constants.RECENT_EVENT_SUMMARY, "~"),
         (constants.RECENT_GRADING_LIST, "~"),
         (constants.RECENT_RATING_LIST, "~"),
+        (ecfformat.core.constants.RECENT_RESULTS_FORMAT_FILE, "~"),
+        (
+            ecfformat.core.constants.SHOW_VALUE_BOUNDARY,
+            ecfformat.core.constants.SHOW_VALUE_BOUNDARY_TRUE,
+        ),
     )
